34ee147bd8c0afecb68a0313736e062d15ae192d
Siddesh Pillai <siddeshvijji@gmail.com>
2016-12-28 17:26:49 -0800

    updated ios local notifications

--- QtNotifier/QtMobileNotification/QtIosNotifier.mm
+++ QtNotifier/QtMobileNotification/QtIosNotifier.mm
@@ -4,28 +4,40 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+@interface NotificationDelegate : UIResponder <UIApplicationDelegate>
+@end
+
+@implementation NotificationDelegate
+        
+- (void)showNotification:(NSString*)title
+                          message:(NSString*)msg;
+{
+    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
+    localNotification.fireDate = [NSDate dateWithTimeIntervalSinceNow:10];
+    localNotification.alertBody = msg;
+    localNotification.timeZone = [NSTimeZone defaultTimeZone];
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+}
+
+@end
+    
 //------------------------------------------------------------------------
 QtIosNotifier::QtIosNotifier()
 {
-    UIUserNotificationType types = UIUserNotificationTypeBadge |
-                 UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
-
-    UIUserNotificationSettings *mySettings =
-                [UIUserNotificationSettings settingsForTypes:types categories:nil];
-
-    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
 
 }
 
 //------------------------------------------------------------------------
 bool QtIosNotifier::show(const QVariant &notificationParameters)
 {
+    NotificationDelegate *mn = [[[NotificationDelegate alloc] init] autorelease];
     QVariantMap parameters = notificationParameters.toMap();
     QString caption = parameters.value("caption", "").toString();
-
+    QString title = parameters.value("title", "").toString();
     UILocalNotification* localNotification = [[UILocalNotification alloc] init];
     localNotification.alertBody = caption.toNSString();
     [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+    [mn showNotification:title.toNSString() message:caption.toNSString()];
     return true;
 }
 //------------------------------------------------------------------------
